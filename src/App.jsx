c2809265c3ee595855883559e68bc60f5ed33ad3
daniel fernandes <danielfernandesdeoliveira459@gmail.com>
2026-05-13 17:19:01 -0400

    usei o opencode para criar um projeto index para trazer umar profissional para esse site.
    separei isso em fork para nao misturar a pratica

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Pratica from "./Pratica"
 function App() {
   return (
     <div>
-      <Pratica que="Pratica"/>
-      <Pratica />
-      <Pratica />
-      <Pratica />
+      <Pratica cor="purple" que="Pratica"/>
+      <Pratica cor="green"/>
+      <Pratica cor="gray"/>
+      <Pratica cor="fuchsia"/>
     </div>
   )
 }
--- src/Pratica.jsx
+++ src/Pratica.jsx
@@ -1,16 +1,42 @@
 import { useState } from "react";
-function Pratica({ que }) {
-    const [Texto, SetTexto] = useState("come minha bunda seu policial")
-    let nome = "desgraça"
-    const UrlImg = "src/imagem/Imagem colada.png"
+function Pratica({ que, cor }) {
+
+    const [texto, setTexto] = useState("come minha bunda seu policial");
+    let nome = "desgraça";
+    const UrlImg = "src/imagem/Imagem colada.png";
+    const [inputTexto, setInputTexto] = useState("");
+
+    function clicou() {
+        setTexto(inputTexto);
+    }
 
     return (
         <div>
-            <h1>me chama de lord {nome} </h1>
+
+            <h1 style={{ color: cor }}>
+                {texto}
+            </h1>
+
+            <input
+                value={inputTexto}
+                onChange={(e) => setInputTexto(e.target.value)}
+                type="text"
+            />
+
+            <h2>
+                me chama de lord {nome}, por favor {texto}
+            </h2>
+
             <img width={500} src={UrlImg} />
-            <button onClick={() => setTexto("toquei no meu butao")} />
+
+            <br />
+
+            <button onClick={clicou}>
+                toque no meu botao
+            </button>
+
         </div>
-    )
+    );
 }
 
-export default Pratica
+export default Pratica;
