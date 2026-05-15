c117898c8c95b61dd82629b97499c92a4c997611
daniel fernandes <danielfernandesdeoliveira459@gmail.com>
2026-05-15 16:16:01 -0400

    esse foi um teste de rotas que eu fiz, consertei alguns bugs com o opencode e agora irei implementar essa estrutura no fork do opencode

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import Pratica from "./Pratica"
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Teste from './Teste';
+import Sobre from './Sobre';
 
 function App() {
   return (
-    <div>
-      <Pratica cor="purple" que="Pratica"/>
-      <Pratica cor="green"/>
-      <Pratica cor="gray"/>
-      <Pratica cor="fuchsia"/>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Teste />} />
+        <Route path="/sobre" element={<Sobre />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Sobre.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+function Sobre() {
+  return (
+    <div>
+      <h1>helow world kux klux klan</h1>
+      <Link to="/">Ir para a página inicial</Link>
+    </div>
+  );
+}
+
+export default Sobre;
